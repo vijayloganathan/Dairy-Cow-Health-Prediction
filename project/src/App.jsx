ed41ef48ec931a3139ca464ef80d1e83c603a18b
Vijay Loganathan <vijayloganathan2002@gmail.com>
2024-05-29 11:31:34 +0530

    commit

---
+++ project/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Landingpage from "./components/Landingpage";
+
+export default function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Landingpage />} />
+          <Route path="/home/*" element={<Home />} />
+        </Routes>
+      </Router>
+    </>
+  );
+}
---
+++ project/src/components/Home.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import Navbar from "./Navbar";
+import Sidenav from "./Sidenav";
+import cowimg from "../assets/image/cowlogo.png";
+import Society_Details from "./Society_Details";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import CollectionDateShift from "./CollectionDateShift";
+import MemberlistForm from "./MemberlistForm";
+import Milkcollection from "./Milkcollection";
+import Todaycollection from "./Todaycollection";
+import PaymentReport from "./PaymentReport";
+import Healthreportform from "./Healthreportform";
+import Healthreport from "./Healthreport";
+
+export default function Home() {
+  return (
+    <div>
+      <Navbar />
+      <div className="container-well">
+        <div className="row m-1 ">
+          <div className="col-2 bg-primary p-1 " style={{ height: "85vh" }}>
+            <Sidenav />
+          </div>
+          <div
+            className="col-9 p-1 "
+            style={{
+              backgroundImage: `url(${cowimg})`,
+              width: "5vw",
+              backgroundRepeat: "no-repeat",
+              backgroundPosition: "center",
+            }}
+          >
+            <div
+              style={{
+                height: "85vh",
+                width: "82vw",
+                backgroundColor: "rgb(255,255,255,0.8)",
+              }}
+              className="d-flex justify-content-center"
+            >
+              <Routes>
+                <Route path="/" element={<CollectionDateShift />} />
+                <Route path="/memberlist" element={<MemberlistForm />} />
+                <Route path="/societydetails" element={<Society_Details />} />
+                <Route path="/milkcollection" element={<Milkcollection />} />
+                <Route path="/todaycollection" element={<Todaycollection />} />
+                <Route path="/paymentreport" element={<PaymentReport />} />
+                <Route path="/Healthreport" element={<Healthreport />} />
+                <Route
+                  path="/Healthreportform"
+                  element={<Healthreportform />}
+                />
+              </Routes>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
